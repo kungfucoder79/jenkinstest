4a8286c969a1af85dc85aa70069132db358090a1
Tim Bush <tim.bush@onbase.com>
2018-07-02 08:21:12 -0400

    added unit test project, a library, and a more better git ignore

---
+++ jenkinstest.test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace jenkinstest.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

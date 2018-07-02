a9950227dc8714ec32bbe4c0caf45c6555074a25
Tim Bush <tim.bush@onbase.com>
2018-07-02 09:28:44 -0400

    changing code to trigger build

--- jenkinstest/Program.cs
+++ jenkinstest/Program.cs
@@ -6,7 +6,7 @@ namespace jenkinstest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!   ");
         }
     }
 }
